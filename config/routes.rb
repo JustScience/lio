449c5a424dda52320a3a9cc56042698f7c53ad3c
JGalenti <thejustscience@gmail.com>
2017-01-21 23:53:35 -0700

    Add Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	root 'pages#home'
+
+	get '/home', to: 'pages#home', as: 'home'
+	get '/contact', to: 'pages#contact', as: 'contact'
+
 end
