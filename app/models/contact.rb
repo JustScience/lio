7075eb72329a8e06c282fa9e47984c483b0b27ab
JGalenti <thejustscience@gmail.com>
2017-03-25 21:16:05 -0600

    Change Contact Model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < MailForm::Base
 	attribute :name,		:validate => true
-	attribute :email,		:validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+	attribute :email,     	:validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 	attribute :message,		:validate => true
 	attribute :nickname,	:captcha => true
 
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
 		{
 			:subject => "Contact Form",
 			:to => "indeeparts@gmail.com",
-			:from => %("#{:name}" <#{:email}>)
+			:from => %("#{name}" <#{email}>)
 		}
 	end
 end
