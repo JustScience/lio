fe26ed8c2cc50abfbb9be9acd1fcb3466859604e
JGalenti <thejustscience@gmail.com>
2017-03-25 20:26:26 -0600

    Setup SendGrid Mail for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'http://jolio.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'your_sendgrid_username',
+    :password => 'your_sendgrid_password',
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
